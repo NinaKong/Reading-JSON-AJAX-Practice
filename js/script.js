be8603a129d6f818f7fadbf2e889740575cba8fe
NinaSleephead <kx1214@gmail.com>
2016-08-24 22:51:50 -0400

    Reading-JSON-AJAX-Practice
    Practice for reading JSON file using AJAX

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var btn = document.getElementById('firstbtn');
+btn.onlick = function () {
+    var httpRequest; 
+    if(window.XMLHttpRequest) {
+	    httpRequest = new XMLHttpRequest();
+    } else {
+	    httpRequest = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+
+    httpRequest.open('GET', 'data/data.json');
+
+    httpRequest.onreadystatechange = function(){
+	    if((httpRequest.status == 200)&&(httpRequest.readyState==4)) {
+	        var data = JSON.parse(httpRequest.responseText);
+	        var result = '<ul>';
+	        for (var key in data) {
+	            result += '<li>' + data[key].shortname + '</li>';
+	        }
+	        result = '</ul>';
+	        document.getElementById('readData').innerHTML = result;
+	    }
+    }
+    httpRequest.send();
+}
+
